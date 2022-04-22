c29bc329bb5879f04e7377041541112b983f2012
Oreki <2536308429@qq.com>
2022-04-22 14:33:05 -0400

    Add info and splash screen

--- app/src/main/java/com/yhe64/treasurehunt/MainActivity.kt
+++ app/src/main/java/com/yhe64/treasurehunt/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.yhe64.treasurehunt
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.yhe64.treasurehunt.ui.main.MainFragment
+import androidx.preference.PreferenceManager
+import com.yhe64.treasurehunt.TreasurehuntApp.Companion.SHOW_MESSAGE_AT_START
+import com.yhe64.treasurehunt.TreasurehuntApp.Companion.CARD_COLOR
+import com.yhe64.treasurehunt.TreasurehuntApp.Companion.SHOW_POINT_SCORE
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navHostFragment: NavHostFragment
 
+    private val prefs: SharedPreferences by lazy {
+        PreferenceManager.getDefaultSharedPreferences(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_Treasurehunt)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
@@ -25,8 +36,15 @@ class MainActivity : AppCompatActivity() {
             supportActionBar?.let {
                 it.title = when(destination.id){
                     R.id.settingFragment -> getString(R.string.settings)
+                    R.id.infoFragment -> getString(R.string.info)
                     else -> getString(R.string.app_name)
                 }
+            }
+        }
+
+        if (savedInstanceState == null){
+            if (prefs.getBoolean(SHOW_MESSAGE_AT_START, false)){
+                welcomeAlert()
             }
         }
     }
@@ -46,7 +64,22 @@ class MainActivity : AppCompatActivity() {
                 navHostFragment.navController.navigate(R.id.action_mainFragment_to_settingFragment)
                 true
             }
+            R.id.action_go_info ->{
+                navHostFragment.navController.navigate(R.id.action_mainFragment_to_infoFragment)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun welcomeAlert(){
+        val msg = resources.getString(R.string.welcomeMsg)
+        val builder = AlertDialog.Builder(this)
+        with(builder){
+            setTitle(R.string.welcome)
+            setMessage(msg)
+            setPositiveButton(R.string.ok,null)
+            show()
         }
     }
 }
---
+++ app/src/main/java/com/yhe64/treasurehunt/TreasurehuntApp.kt
@@ -0,0 +1,19 @@
+package com.yhe64.treasurehunt
+
+import android.app.Application
+
+class TreasurehuntApp: Application() {
+
+    companion object{
+        private lateinit var instance: TreasurehuntApp
+
+        const val SHOW_MESSAGE_AT_START = "show_message_at_start"
+        const val CARD_COLOR = "card_color"
+        const val SHOW_POINT_SCORE = "show_poit_score"
+    }
+
+    override fun onCreate() {
+        instance = this
+        super.onCreate()
+    }
+}
