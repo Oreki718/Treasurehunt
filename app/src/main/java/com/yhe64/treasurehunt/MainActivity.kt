6cc7255cd1334874361d5cd9253f51bd3aec2591
Oreki <2536308429@qq.com>
2022-04-21 20:18:18 -0400

    init

---
+++ app/src/main/java/com/yhe64/treasurehunt/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.yhe64.treasurehunt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yhe64.treasurehunt.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
